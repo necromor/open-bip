5a96b1557122253992b7c3ddacc1a9a9a1b157e1
Michal Jewczuk <michal@jewczuk.net>
2019-04-18 08:17:21 +0200

    spring boot initial configuration

---
+++ src/main/java/net/jewczuk/projektbip/ProjektBipApplication.java
@@ -0,0 +1,13 @@
+package net.jewczuk.projektbip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektBipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektBipApplication.class, args);
+	}
+
+}
