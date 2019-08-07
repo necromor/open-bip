da317c15e94663c4709d2b3afba2149c6fcd9d0e
Michal Jewczuk <michal@jewczuk.net>
2019-08-07 13:39:47 +0200

    Add Article - added basic functionality to add article; no validation; no error messages

--- src/main/java/net/jewczuk/openbip/controller/PanelController.java
+++ src/main/java/net/jewczuk/openbip/controller/PanelController.java
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.jewczuk.openbip.constants.ViewNames;
+import net.jewczuk.openbip.exceptions.BusinessException;
 import net.jewczuk.openbip.exceptions.ResourceNotFoundException;
 import net.jewczuk.openbip.service.ArticleService;
 import net.jewczuk.openbip.to.ArticleLinkTO;
 import net.jewczuk.openbip.to.DisplaySingleArticleTO;
+import net.jewczuk.openbip.utils.TransformUtils;
 
 @Controller
 @RequestMapping("/panel")
@@ -61,9 +64,27 @@ public class PanelController {
 	}
 	
 	@GetMapping("/dodaj-artykul")
-	public String showAddArticle() {
+	public String showAddArticle(Model model) {
+		
+		DisplaySingleArticleTO article = (DisplaySingleArticleTO) model.asMap().getOrDefault("newArticle", new DisplaySingleArticleTO());
+		model.addAttribute("newArticle", article);
 		
 		return ViewNames.ARTICLE_ADD;
+	}
+	
+	@PostMapping("/dodaj-artykul.do")
+	public String addArticle(Model model, DisplaySingleArticleTO newArticle) {
+		
+		newArticle.setLink(TransformUtils.createLinkFromTitle(newArticle.getTitle()));
+		DisplaySingleArticleTO savedArticle;
+		try {
+			savedArticle = articleService.saveArticle(newArticle);
+			return "redirect:/panel/zarzadzaj/" + savedArticle.getLink();
+		} catch (BusinessException e) {
+			
+			model.addAttribute("newArticle", newArticle);
+			return ViewNames.ARTICLE_ADD;
+		}	
 	}
 
 }
