c83c13a78b7edba45518a84cf70fcaea1fe18931
Michal Jewczuk <michal@jewczuk.net>
2019-08-28 09:57:15 +0200

    Sandbox ajax save - implemented rest endpoint to use sandbox service to save changes

--- src/main/java/net/jewczuk/openbip/rest/controller/SandboxRestController.java
+++ src/main/java/net/jewczuk/openbip/rest/controller/SandboxRestController.java
@@ -1,30 +1,37 @@
 package net.jewczuk.openbip.rest.controller;
 
-import java.util.Random;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.jewczuk.openbip.constants.UIMessages;
+import net.jewczuk.openbip.exceptions.BusinessException;
 import net.jewczuk.openbip.rest.objects.AjaxGenericResponse;
+import net.jewczuk.openbip.service.SandboxService;
 import net.jewczuk.openbip.to.SandboxTO;
 
 @RestController
 @RequestMapping("/api/sandbox")
 public class SandboxRestController {
 	
+	@Autowired
+	private SandboxService sandboxService;
+	
 	@PutMapping("/save")
 	public AjaxGenericResponse saveSandbox(@RequestBody SandboxTO sandbox) {
+		Long editorID = 1L;
 		
-		System.out.println("title: " + sandbox.getTitle());
-		System.out.println("link: " + sandbox.getLink());
-		System.out.println("content: " + sandbox.getContent());
-		
-		//tmp	
-		boolean result = new Random().nextBoolean();
-		String message = result ? UIMessages.SAVE_SANDBOX_FAILURE : UIMessages.SAVE_SANDBOX_SUCCESS;
+		boolean result = false;
+		String message = UIMessages.SAVE_SANDBOX_SUCCESS;
+		try {
+			sandboxService.editSandbox(sandbox, editorID);	
+		} catch (BusinessException e) {	
+			message = UIMessages.SAVE_SANDBOX_FAILURE;
+			result = true;
+		}	
+
 		return new AjaxGenericResponse(result, message);
 	}
 
