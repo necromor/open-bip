d5e5c0e4860066ef854341e48542d223e143df60
Michal Jewczuk <michal@jewczuk.net>
2019-08-30 12:01:22 +0200

    Refactoring - moved jquery functions to panel_main.js; added new method in panel_article in preparation to ajax calls to api to save new positions

--- src/main/resources/static/js/panel_article.js
+++ src/main/resources/static/js/panel_article.js
@@ -4,7 +4,12 @@ let panel_article = (function () {
 	
 	const titleField = document.getElementById('title');
 	const linkField = document.getElementById('link');
-	
+	const mainMenuSymbol = 'main-menu-elements';
+	const childrenSymbol = 'children';
+	const attachmentsSymbol = 'attachments';
+	const mainMenuPositions = '/api/main-menu-positions';
+	const articleChildrenPositions = '/api/children-positions/';
+	const articleAttachmentsPositions = '/api/attachments-positions/';
 	
 	function registerCreateLinkEvent() {
 		if (titleField) {
@@ -28,10 +33,37 @@ let panel_article = (function () {
 
         return str;
 	}
+	
+	function extractArticleLink(address) {
+		let splitted = address.split('/');
+		return splitted[splitted.length - 1];
+	}
+	
+	function saveNewPositions(name, links) {
+		const link = returnLinkBasedOnName(name);
+		console.log(name);
+		console.log(link);
+		console.log(links);
+	}
 
+	function returnLinkBasedOnName(name) {
+		if (name === mainMenuSymbol) {
+			return mainMenuPositions;
+		} else {
+			const splitted = name.split('#');
+			if (splitted[1] === childrenSymbol) {
+				return articleChildrenPositions + splitted[0];
+			} else {
+				return articleAttachmentsPositions + splitted[0];
+			}
+		}
+	}
+	
 	
 	return {
-		registerCreateLinkEvent: registerCreateLinkEvent
+		registerCreateLinkEvent: registerCreateLinkEvent,
+		extractArticleLink: extractArticleLink,
+		saveNewPositions: saveNewPositions
 	}
 	
 })();
--- src/main/resources/static/js/panel_main.js
+++ src/main/resources/static/js/panel_main.js
@@ -2,3 +2,17 @@
 
 panel_article.registerCreateLinkEvent();
 panel_sandbox.registerSandboxEvent();
+
+$( function() {
+	  $( ".sortable-list" ).sortable({
+	      stop: function() {
+	    	  const name = this.id;
+	    	  const links = [];
+	    	    $(this).find('li a.link-list-element-link').each(function(){
+	    	    	links.push(panel_article.extractArticleLink(this.href))
+	    	    });
+	    	  panel_article.saveNewPositions(name, links);
+	      }
+	    });
+  $( ".sortable-list" ).disableSelection();
+} );
