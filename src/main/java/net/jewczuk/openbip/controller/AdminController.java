a5b37a419a8cbe2fd644d7e9b15529247f47948e
Michal Jewczuk <michal@jewczuk.net>
2019-09-16 14:03:49 +0200

    Admin - created main admin page with list of editors

--- src/main/java/net/jewczuk/openbip/controller/AdminController.java
+++ src/main/java/net/jewczuk/openbip/controller/AdminController.java
@@ -1,16 +1,24 @@
 package net.jewczuk.openbip.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.jewczuk.openbip.constants.ViewNames;
+import net.jewczuk.openbip.service.EditorService;
+import net.jewczuk.openbip.to.EditorTO;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	private EditorService editorService;
+	
 	@GetMapping("")
 	public String redirectToMainAdminPage() {	
 		return "redirect:/admin/";
@@ -18,6 +26,8 @@ public class AdminController {
 	
 	@GetMapping("/")
 	public String showMainAdminPage(Model model) {
+		List<EditorTO> editors = editorService.getAllEditorsOnly();
+		model.addAttribute("editors", editors);
 		
 		return ViewNames.ADMIN_MAIN;
 	}
