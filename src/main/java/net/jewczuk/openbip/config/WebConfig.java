1bdbde68871701e71cd7845b3ad4da74099f2eed
Michal Jewczuk <michal@jewczuk.net>
2019-10-07 09:35:07 +0200

    Created company.properties file to store full and short name; added names to templates

--- src/main/java/net/jewczuk/openbip/config/WebConfig.java
+++ src/main/java/net/jewczuk/openbip/config/WebConfig.java
@@ -1,6 +1,9 @@
 package net.jewczuk.openbip.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,5 +23,14 @@ public class WebConfig implements WebMvcConfigurer {
                         "classpath:/static/css/",
                         "classpath:/static/js/");
     }
-
+  
+    @Bean
+    public MessageSource messageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/messages", "classpath:/company");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(5);
+        return messageSource;
+    }
 }
